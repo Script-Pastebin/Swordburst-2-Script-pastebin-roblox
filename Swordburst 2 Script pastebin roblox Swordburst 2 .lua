eaaeafb90337445ffbb2e7b4dd015c4906ecf2b5
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:58:43 +0100

    commit zero

---
+++ Swordburst 2 Script pastebin roblox Swordburst 2 .lua
@@ -0,0 +1,10 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(('https://pastebin.com/raw/F5vSFHZt'),true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+Cool script for Swordburst 2
+very functional and not only :)
+]]
